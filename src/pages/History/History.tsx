f67198d526507dda5384f00cee449914c4a54f5b
preykriti <prakriteeshrestha2@gmail.com>
2025-12-04 08:01:45 +0545

    feat: add history page to show past transactions

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import styles from "./History.module.css";
+import { useTransactionContext } from "../../context/TransactionContext";
+import type { Transaction } from "../../types/Transaction";
+import TransactionCard from "../../components/TransactionCard/TransactionCard";
+import InputForm from "../../components/InputForm/InputForm";
 
 const History = () => {
-  return (
-    <div>History</div>
-  )
-}
+  const [showForm, setShowForm] = useState<boolean>(false);
+  const [editTransaction, setEditTransaction] = useState<Transaction | null>(
+    null
+  );
+  const { transactions } = useTransactionContext();
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
-export default History
+  const toggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
+  const handleEdit = (id: string) => {
+    const tx = transactions.find((t) => t.id === id);
+    if (tx) {
+      setEditTransaction(tx);
+      setShowForm(true);
+    }
+  };
+
+  return (
+    <div className={styles.historyContainer}>
+      <div className={styles.topContainer}>
+
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className={styles.searchInput}
+        />
+      </div>
+      <TransactionCard onEdit={handleEdit} searchQuery={searchQuery} />
+    </div>
+  );
+};
+
+export default History;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,63 +1,12 @@
-import { useState } from "react"
-import InputForm from "../../components/InputForm/InputForm"
 import styles from "./Home.module.css"
 import Totals from "../../components/Totals/Totals"
-import TransactionCard from "../../components/TransactionCard/TransactionCard"
-import type { Transaction } from "../../types/Transaction"
-import { useTransactionContext } from "../../context/TransactionContext"
-import Navbar from "../../components/Navbar/Navbar"
-
 const Home = () => {
-    const [showForm, setShowForm] = useState<boolean>(false);
-    const [editTransaction, setEditTransaction] = useState<Transaction | null> (null);
-    const {transactions}  = useTransactionContext();
-    const [searchQuery, setSearchQuery] = useState<string>("");
-
-
-    const toggleForm = () => {  
-        setShowForm(prev => !prev);
-    }
-
-    const handleEdit = (id: string)=>{
-         const tx = transactions.find(t => t.id === id);
-        if (tx) {
-            setEditTransaction(tx);
-            setShowForm(true);
-        }
-    }
 
 
   return (
-    <div>
-       
-        <Navbar />
+    <div className={styles.homeContainer}>
+        {/* <Navbar /> */}
         <Totals />
-        <div className={styles.topContainer}>
-            <button className={styles.addBtn} onClick={toggleForm}>Add</button>
-            
-                <input
-                    type="text"
-                    placeholder="Search"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className={styles.searchInput}
-                />
-         
-        </div>
-        <TransactionCard onEdit={handleEdit} searchQuery={searchQuery}/>
-
-        {showForm && (
-            <div className={styles.formContainer} onClick={toggleForm}>
-                <div className={styles.form} onClick={(e) => e.stopPropagation()}>
-                    <InputForm onClose={()=>{setShowForm(false) 
-                        setEditTransaction(null);
-                    }} editableTransaction={editTransaction}/>
-                </div>
-            </div>
-
-        )}
-
-
     </div>
   )
 }
